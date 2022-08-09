8cee39b118cfbac8a49dfec065b485a91aca13a4
Raphael <raphael.soula@gmail.com>
2022-08-09 10:59:02 +0200

    Dernière version du site, fonctionnelle. (Normalement identique à la dernière version de la version de formation)

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,15 +1,41 @@
-function CareScale(props) {
-    const {scaleValue, careType} = props
-    
-    const scaleType = careType === 'light' ? '☀️' : '💧'
+import Sun from '../assets/sun.svg'
+import Water from '../assets/water.svg'
 
-    const range = [1,2,3]
-
-    return <div>
-        {range.map((rangeElem) =>
-            scaleValue >= rangeElem ? <span key={rangeElem.toString()}>{scaleType}</span> : null
-        )}
-    </div>
+const quantityLabel = {
+	1: 'peu',
+	2: 'moderement',
+	3: 'beaucoup'
 }
+
+function CareScale({scaleValue, careType}) {
+
+	const range = [1, 2, 3]
+    
+	const scaleType =
+		careType === 'light' ? (
+			<img src={Sun} alt='sun-icon' />
+		) : (
+			<img src={Water} alt='water-icon' />
+		)
+
+	return (
+		<div
+			onClick={() =>
+				alert(
+					`Cette plante requiert ${quantityLabel[scaleValue]} ${
+						careType === 'light' ? 'de lumiere' : "d'arrosage"
+					}`
+				)
+			}
+		>
+			{range.map((rangeElem) =>
+				scaleValue >= rangeElem ? (
+					<span key={rangeElem.toString()}>{scaleType}</span>
+				) : null
+			)}
+		</div>
+	)
+}
+
 
 export default CareScale
