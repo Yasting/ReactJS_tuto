1213863c11e4130a0d80388ce401d233be7d5945
Raphael <raphael.soula@gmail.com>
2022-08-09 10:08:34 +0200

    -

---
+++ src/components/CareScale.js
@@ -0,0 +1,15 @@
+function CareScale(props) {
+    const {scaleValue, careType} = props
+    
+    const scaleType = careType === 'light' ? '☀️' : '💧'
+
+    const range = [1,2,3]
+
+    return <div>
+        {range.map((rangeElem) =>
+            scaleValue >= rangeElem ? <span key={rangeElem.toString()}>{scaleType}</span> : null
+        )}
+    </div>
+}
+
+export default CareScale
---
+++ src/datas/plantList.js
@@ -0,0 +1,74 @@
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+		isBestSale: false,
+		light:2,
+		water:1
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+		isBestSale: false,
+		light:1,
+		water:1
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		isBestSale: false,
+		light:1,
+		water:1
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk',
+		isBestSale: false,
+		light:3,
+		water:2
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+		isBestSale: false,
+		light:1,
+		water:1
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo',
+		isBestSale: false,
+		light:1,
+		water:3
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		isBestSale: true,
+		light:3,
+		water:1
+	},
+	{
+		name: 'aloe',
+		category: 'plante grasse',
+		id: '8fp',
+		isBestSale: false,
+		light:1,
+		water:1
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+		isBestSale: false,
+		light:2,
+		water:1
+	}
+]
